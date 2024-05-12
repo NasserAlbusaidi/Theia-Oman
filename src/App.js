18e9267ed873e8c3a9140960425ac45a3d79e2d3
NasserAlbusaidi <nasserbusaidi@gmail.com>
2024-05-12 21:55:55 +0400

    chore: Add inventory management section to App.js

--- src/App.js
+++ src/App.js
@@ -93,6 +93,24 @@ function App() {
           </ul>
         </div>
       </nav>
+      <div className="pt-4 flex flex-row justify-center items-center text-white bg-gray-200">
+      <div className="w-1/2 p-8 text-left ml-8"> 
+        <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold text-gray-800">
+          Inventory <br /> Management <br /> Application
+        </h1>
+        <p className="text-lg md:text-xl lg:text-2xl text-gray-800 mt-4">
+          Your Supply chain, Optimized
+        </p>
+      </div>
+
+      <div className="w-1/2 pb-2">
+        <img
+          src="/images/inventory.jpg"
+          alt="Header"
+          className="w-100 h-100 object-cover"
+        />
+      </div>
+    </div>
       <section
         className="bg-white py-12 px-4 md:px-8 lg:px-16"
         ref={whatWeDoRef}
